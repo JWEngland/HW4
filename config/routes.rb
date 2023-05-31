894d41f55918045ca0715341f57f915d2b3ab393
James England <jimmy.england@kellogg.northwestern.edu>
2023-05-31 16:33:25 +0000

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'registrations/new'
+  get 'registrations/create'
+  get 'user_sessions/new'
+  get 'user_sessions/create'
   get "/", :controller => "places", :action => "index"
   resources "places"
   resources "posts"
   resources "sessions"
   resources "users"
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  get '/signup', to: 'registrations#new'
+  post '/signup', to: 'registrations#create'
+
+
 end
