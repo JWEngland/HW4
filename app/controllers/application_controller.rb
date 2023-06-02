7f263d72e11c6d5f78eb4ab28e393d664f331afe
James England <jimmy.england@kellogg.northwestern.edu>
2023-06-02 03:10:37 +0000

    working - have to log in to create a new place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   def current_user
     puts "------------------ code before every request ------------------"
   end
+
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user_instance = User.find_by(id: session[:user_id])
+  end
+  
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
