d72de11baef8e223f825e5583a2ab996bae8a728
James England <jimmy.england@kellogg.northwestern.edu>
2023-06-05 23:49:25 +0000

    working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
   def create
-    @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
-    @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
-    @user.save
-    redirect_to "/"
+    @user = User.new(user_params)
+  
+    if @user.save
+      # Handle successful user creation
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      # Handle validation errors
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  class User < ApplicationRecord
+    attr_accessor :first_name
+    # or
+    # attr_accessible :first_name
+  end
 end
