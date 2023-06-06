d400a12fab8a6ad4f7bad4e5e25fc2fc414b85e5
James England <jimmy.england@kellogg.northwestern.edu>
2023-06-06 20:48:16 +0000

    working

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,30 +2,25 @@ class UserSessionsController < ApplicationController
   def new
     @user = User.new
   end
-  
 
   def create
     @user = User.find_by(email: params[:user][:email])
-  
+
     if @user&.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to places_path, notice: 'Logged in successfully.'
     else
-      flash.now[:alert] = 'Invalid email or password.'
-      render :new
+      if @user.nil?
+        flash.now[:alert] = 'Invalid email.'
+      else
+        flash.now[:alert] = 'Invalid password.'
+      end
+      render :new, status: :unprocessable_entity
     end
   end
-  
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, notice: 'Logged out successfully.'
-  end
-  
-
-  private
-
-  def user_session_params
-    params.require(:user_session).permit(:email, :password)
+    redirect_to root_path, notice: 'Logged out successfully.'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'user_sessions#new', as: 'login'
   post '/login', to: 'user_sessions#create'
-  delete '/logout', to: 'user_sessions#destroy', as: :logout
+  get '/logout', to: 'user_sessions#destroy', as: :logout
 
   resources :users, only: [:new, :create]
 end
