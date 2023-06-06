3cdbcd62cf983e197562789c32dae176d0e83f58
James England <jimmy.england@kellogg.northwestern.edu>
2023-06-06 21:36:41 +0000

    error messages for invalid email and password

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,12 +1,14 @@
 class UserSessionsController < ApplicationController
+  rescue_from ActionController::RoutingError, with: :handle_routing_error
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-
-    if @user&.authenticate(params[:user][:password])
+    @user = User.find_by(email: params[:email])
+  
+    if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to places_path, notice: 'Logged in successfully.'
     else
@@ -18,9 +20,16 @@ class UserSessionsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Logged out successfully.'
+  end
+  private
+
+  def handle_routing_error
+    flash[:error] = 'Invalid username.'
+    redirect_to login_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   resources :places
   resources :posts
+  resources :user_sessions, only: [:new, :create, :destroy]
 
   get '/login', to: 'user_sessions#new', as: 'login'
   post '/login', to: 'user_sessions#create'
   get '/logout', to: 'user_sessions#destroy', as: :logout
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create], path_names: { new: 'signup' }
+  get '/users/new', to: 'users#new', as: 'new_user'
 end
