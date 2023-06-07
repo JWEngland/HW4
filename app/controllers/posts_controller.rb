549b0baa836a33d882e662de6ff9a572ff0a6486
James England <jimmy.england@kellogg.northwestern.edu>
2023-06-07 03:35:01 +0000

    Bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,27 +27,4 @@ class PostsController < ApplicationController
       redirect_to login_path
     end
   end
-  def show
-    @place = Place.find(params[:id])
-    @post = Post.new
-    @posts = @place.posts
-  end
-
-  def create_post
-    @place = Place.find(params[:id])
-    @post = @place.posts.build(post_params)
-
-    if @post.save
-      redirect_to place_path(@place), notice: 'Post was successfully created.'
-    else
-      render :show
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :description, :date_posted, :image)
-  end
 end
-
